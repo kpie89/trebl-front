41765b834e3a79c4423e79491df53ce2b15c8f87
kpie89 <kpie89@gmail.com>
2016-04-06 13:47:29 -0400

    intial model for song

---
+++ app/song/model.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  duration: DS.attr('number'),
+  soundcloud_id: DS.attr('number'),
+
+  playlists: DS.hasMany('playlists', {async: true})
+});
