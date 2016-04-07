9e835c2f980eb3d4d2f42bd21edf0d0e9cf2721d
kpie89 <kpie89@gmail.com>
2016-04-06 20:35:40 -0400

    add song search route

--- app/song-search/route.js
+++ app/song-search/route.js
@@ -6,19 +6,24 @@ export default Ember.Route.extend({
 
   actions: {
     songSearch(key) {
+      this.get('search').songSearch(key);
+    },
+    createSongAndPlaylist(soundcloudSong) {
       console.log('here');
-      console.log(key);
-
-      this.get('search').songSearch(key)
-      .then(() => {
-        console.log(key);
-        this.get('flashMessages')
-        .success('You did something you freak');
-      })
-      .catch(() => {
-        this.get('flashMessages')
-        .danger('Try again dummy.');
+      console.log('Song',soundcloudSong);
+      let songRecord = this.store.createRecord('song', {
+        title: soundcloudSong.title,
+        duration: soundcloudSong.duration,
+        soundcloud_id: soundcloudSong.id
       });
+      let playlistRecord = null;
+      songRecord.save()
+        .then(() => {
+          playlistRecord = this.store.createRecord('playlist');
+          songRecord.get('playlists').pushObject(playlistRecord); // set song_id
+          return playlistRecord.save();
+        })
+        .then(() => this.transitionTo('playlist', playlistRecord));
     }
   }
 });
