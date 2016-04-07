3663d79b664230d18369d18e26e771a92da975e6
kpie89 <kpie89@gmail.com>
2016-04-06 20:32:36 -0400

    generate playlists route and model

--- app/playlists/route.js
+++ app/playlists/route.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return Ember.RSVP.hash({
+      playlist: this.store.findAll('playlist'),
+      song: this.store.findAll('song'),
+      user: this.store.findAll('user')
+    });
+  },
+  actions: {
+    deletePlaylist: function(playlist){
+      playlist.destroyRecord();
+    }
+
+  }
 });
