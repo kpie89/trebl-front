958424664e0080ad003dff42b63df9651151ec4f
kpie89 <kpie89@gmail.com>
2016-04-06 20:35:04 -0400

    add service to search

--- app/search/service.js
+++ app/search/service.js
@@ -2,16 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   ajax: Ember.inject.service(),
+  songResults: {},
 
   songSearch(key) {
-    console.log('here bitch');
-    console.log(key);
     return this.get('ajax').request('/song', {
       data: {
         key
       },
     }).then((result) => {
-      console.log(result);
+      this.set('songResults', result);
     });
   },
 });
