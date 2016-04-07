4deab15914db908941f1c4008153e055675d1c8a
kpie89 <kpie89@gmail.com>
2016-04-07 10:12:50 -0400

    add buttons for play and view playlist

--- app/components/playlist-snippet/component.js
+++ app/components/playlist-snippet/component.js
@@ -1,12 +1,34 @@
 import Ember from 'ember';
+import SC from 'npm:soundcloud';
 
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
+  search: Ember.inject.service(),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  isCommenting: false,
 
   actions: {
     deletePlaylist () {
       this.sendAction('deletePlaylist', this.get('playlist'));
-    }
-  }
+    },
+    willComment () {
+      this.toggleProperty('isCommenting');
+    },
+    songSearch (key) {
+      this.get('search').songSearch(key);
+    },
+    reset () {
+      this.set('search', {});
+    },
+    createCommentAndSong(song) {
+      this.sendAction('createCommentAndSong', song, this.get('playlist'));
+    },
+    viewPlaylist(params) {
+      this.transitionTo('playlist', params.playlist_id);
+  },
+  playTrack(songId) {
+    console.log(songId);
+    SC.stream(`/tracks/${songId}`).then((player) => player.play()).catch();
+  },
+ }
 });
