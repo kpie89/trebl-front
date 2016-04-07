67e01beb1d0d4d517c95e2b79517b6d883f953ec
kpie89 <kpie89@gmail.com>
2016-04-07 10:15:33 -0400

    update template for new buttons

--- app/playlists/route.js
+++ app/playlists/route.js
@@ -12,6 +12,25 @@ export default Ember.Route.extend({
   actions: {
     deletePlaylist: function(playlist){
       playlist.destroyRecord();
+    },
+    createCommentAndSong (soundcloudSong, playlist) {
+      console.log('here');
+      let songRecord = this.get('store').createRecord('song', {
+          title: soundcloudSong.title,
+          duration: soundcloudSong.duration,
+          soundcloud_id: soundcloudSong.id
+        });
+        let commentRecord = null;
+        songRecord.save()
+          .then(() => {
+            commentRecord = this.store.createRecord('comment');
+
+            songRecord.get('comments').pushObject(commentRecord);
+            console.log(playlist);
+            playlist.get('comments').pushObject(commentRecord);// set song_i
+            return commentRecord.save();
+          })
+          .then(() => this.transitionTo('playlists'));
     }
 
   }
